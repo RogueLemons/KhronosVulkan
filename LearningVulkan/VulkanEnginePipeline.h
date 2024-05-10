60c88db3b7d5d721be45aa1f0ac1cf55f3163452
ludvig.langebro@gmail.com <ludvig.langebro@gmail.com>
2024-05-10 17:20:56 +0200

    Add initial testing shaders and pipeline class

---
+++ LearningVulkan/VulkanEnginePipeline.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <string>
+#include <vector>
+
+class VulkanEnginePipeline
+{
+public:
+	VulkanEnginePipeline(const std::string& vertFilepath, const std::string& fragFilepath);
+private:
+	static std::vector<char> readFile(const std::string& filepath);
+	void createGraphicsPipeline(const std::string& vertFilepath, const std::string& fragFilepath);
+};
+
