70cb69155ec74dde313e1f2fa967b5900eb110ae
ludvig.langebro@gmail.com <ludvig.langebro@gmail.com>
2024-05-15 09:33:59 +0200

    Place shader compiler with shaders

--- LearningVulkan/shaders/compile.bat
+++ LearningVulkan/shaders/compile.bat
@@ -1,3 +1,5 @@
-..\..\..\downloads\VulkanSDK\1.3.280.0\Bin\glslc.exe simple_shader.vert -o simple_shader.vert.spv
-..\..\..\downloads\VulkanSDK\1.3.280.0\Bin\glslc.exe simple_shader.frag -o simple_shader.frag.spv
+:: ..\..\..\downloads\VulkanSDK\1.3.280.0\Bin\glslc.exe simple_shader.vert -o simple_shader.vert.spv
+:: ..\..\..\downloads\VulkanSDK\1.3.280.0\Bin\glslc.exe simple_shader.frag -o simple_shader.frag.spv
+glslc.exe simple_shader.vert -o simple_shader.vert.spv
+glslc.exe simple_shader.frag -o simple_shader.frag.spv
 pause
