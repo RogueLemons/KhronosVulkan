2acd95b5b143a959b42ef7582801cc5bb67a2d01
ludvig.langebro@gmail.com <ludvig.langebro@gmail.com>
2024-05-22 17:20:28 +0200

    Add echo to shader compilation

--- LearningVulkan/shaders/compile.bat
+++ LearningVulkan/shaders/compile.bat
@@ -1,6 +1,8 @@
 :: ..\..\..\downloads\VulkanSDK\1.3.280.0\Bin\glslc.exe simple_shader.vert -o simple_shader.vert.spv
 :: ..\..\..\downloads\VulkanSDK\1.3.280.0\Bin\glslc.exe simple_shader.frag -o simple_shader.frag.spv
 
+
+echo Shader compilation
 set currentDir=%0\..
 glslc.exe %currentDir%\simple_shader.vert -o %currentDir%\simple_shader.vert.spv
 glslc.exe %currentDir%\simple_shader.frag -o %currentDir%\simple_shader.frag.spv
