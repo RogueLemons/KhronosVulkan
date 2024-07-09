780d4964db0095cf2df5d142ce7c158251e3cef2
ludvig.langebro@gmail.com <ludvig.langebro@gmail.com>
2024-07-09 18:42:34 +0200

    Reenable present modes

--- LearningVulkan/LVE_SwapChain.cpp
+++ LearningVulkan/LVE_SwapChain.cpp
@@ -384,19 +384,19 @@ namespace LVE {
 
     VkPresentModeKHR LVE_SwapChain::chooseSwapPresentMode(
         const std::vector<VkPresentModeKHR>& availablePresentModes) {
-        // for (const auto& availablePresentMode : availablePresentModes) {
-        //     if (availablePresentMode == VK_PRESENT_MODE_MAILBOX_KHR) {
-        //         std::cout << "Present mode: Mailbox" << std::endl;
-        //         return availablePresentMode;
-        //     }
-        // }
+         for (const auto& availablePresentMode : availablePresentModes) {
+             if (availablePresentMode == VK_PRESENT_MODE_MAILBOX_KHR) {
+                 std::cout << "Present mode: Mailbox" << std::endl;
+                 return availablePresentMode;
+             }
+         }
 
-        // for (const auto &availablePresentMode : availablePresentModes) {
-        //   if (availablePresentMode == VK_PRESENT_MODE_IMMEDIATE_KHR) {
-        //     std::cout << "Present mode: Immediate" << std::endl;
-        //     return availablePresentMode;
-        //   }
-        // }
+         for (const auto &availablePresentMode : availablePresentModes) {
+           if (availablePresentMode == VK_PRESENT_MODE_IMMEDIATE_KHR) {
+             std::cout << "Present mode: Immediate" << std::endl;
+             return availablePresentMode;
+           }
+         }
 
         std::cout << "Present mode: V-Sync" << std::endl;
         return VK_PRESENT_MODE_FIFO_KHR;
